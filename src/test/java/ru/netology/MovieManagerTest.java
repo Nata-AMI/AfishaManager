207ba7f4b5d65938658ce7d78cac5b7fb186b0d6
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-28 15:16:25 +0500

    6

--- src/test/java/ru/netology/MovieManagerTest.java
+++ src/test/java/ru/netology/MovieManagerTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MovieManagerTestNonEmpty {
+    MovieManager manager = new MovieManager();
+    Movie first = new Movie(1, 1, "first", 1, 1);
+    Movie second = new Movie(2, 2, "second", 1, 1);
+    Movie third = new Movie(3, 3, "third", 1, 1);
     @Test
     public void shouldRemoveIfExists() {
-        MovieManager manager = new MovieManager();
         int idToRemove = 1;
-        Movie first = new Movie(1, 1, "first", 1, 1);
-        Movie second = new Movie(2, 2, "second", 1, 1);
-        Movie third = new Movie(3, 3, "third", 1, 1);
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -27,11 +27,7 @@ public class MovieManagerTestNonEmpty {
 
     @Test
     public void shouldNotRemoveIfNotExists() {
-        MovieManager manager = new MovieManager();
         int idToRemove = 4;
-        Movie first = new Movie(1, 1, "first", 1, 1);
-        Movie second = new Movie(2, 2, "second", 1, 1);
-        Movie third = new Movie(3, 3, "third", 1, 1);
         manager.add(first);
         manager.add(second);
         manager.add(third);
